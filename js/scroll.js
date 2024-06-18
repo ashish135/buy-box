4dfafa077ec1932db087f09dad7118f2d51143fd
Ashish Tomar <ashishtomar@Ashishs-MacBook-Pro.local>
2024-06-18 15:30:26 +0530

    Sticky Buy box

--- js/scroll.js
+++ js/scroll.js
@@ -11,32 +11,33 @@ let isSticky = false;
     const bottomVisible = rightColumnRect.bottom <= windowHeight;
     const topVisible = rightColumnRect.top > 0;
     const leftColVisible = leftColumnRect.top > 0;
-  
-    if( currentScroll > lastScroll && bottomVisible){
-        rightColumn.style.position = 'sticky'
-        isSticky = true;
-        rightColumn.style.top = -parseInt(rightColumnRect.height-windowHeight)+'px'
-    }
-    if( currentScroll > lastScroll && isSticky && rightColumnRect.top === 0){
-        rightColumn.style.position = 'relative'
-        isSticky = false
-        rightColumn.style.top = parseInt(currentScroll)+'px'
-    }
-    if( currentScroll < lastScroll && bottomVisible) {
-        rightColumn.style.position = 'relative'
-        isSticky = false
-        rightColumn.style.top = parseInt(currentScroll+rightColumnRect.top)+'px'
-    } else {
-        if(currentScroll < lastScroll && topVisible){
-          rightColumn.style.position = 'sticky'
-          rightColumn.style.top = 0
-          isSticky = true;
-      }if(currentScroll < lastScroll && leftColVisible){
-          rightColumn.style.position = 'relative'
-          rightColumn.style.top = 0
-          isSticky = false;
-      }
-    }
+    if(rightColumnRect.height > windowHeight){
+        if( currentScroll > lastScroll && bottomVisible){
+            rightColumn.style.position = 'sticky'
+            isSticky = true;
+            rightColumn.style.top = -parseInt(rightColumnRect.height-windowHeight)+'px'
+        }
+        if( currentScroll > lastScroll && isSticky && rightColumnRect.top === 0){
+            rightColumn.style.position = 'relative'
+            isSticky = false
+            rightColumn.style.top = parseInt(currentScroll)+'px'
+        }
+        if( currentScroll < lastScroll && bottomVisible) {
+            rightColumn.style.position = 'relative'
+            isSticky = false
+            rightColumn.style.top = parseInt(currentScroll+rightColumnRect.top)+'px'
+        } else {
+            if(currentScroll < lastScroll && topVisible){
+            rightColumn.style.position = 'sticky'
+            rightColumn.style.top = 0
+            isSticky = true;
+        }if(currentScroll < lastScroll && leftColVisible){
+            rightColumn.style.position = 'relative'
+            rightColumn.style.top = 0
+            isSticky = false;
+        }
+        }
 
-    lastScroll = currentScroll;
+        lastScroll = currentScroll;
+    }
   });
